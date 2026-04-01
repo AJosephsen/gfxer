3ee9b1281963a8f6cb1060056c3a68b8b824eb44
AJosephsen <34395102+AJosephsen@users.noreply.github.com>
2026-04-01 06:59:18 +0000

    feat: add Game class with name and guid

--- StrategyCards/src/core/game.ts
+++ StrategyCards/src/core/game.ts
@@ -5,3 +5,13 @@ export type GameState = {
 export function createInitialGameState(): GameState {
   return { turn: 1 };
 }
+
+export class Game {
+  public readonly guid: string;
+  public readonly name: string;
+
+  public constructor(name: string) {
+    this.name = name;
+    this.guid = crypto.randomUUID();
+  }
+}
